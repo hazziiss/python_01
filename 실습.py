9a3019a6e43b76529a117979b731ee9b81b74fcc
hazziiss <lhj25846@naver.com>
2023-01-02 17:06:19 +0900

    python 실습 1주차 01

---
+++ 실습.py
@@ -0,0 +1,92 @@
+#1
+# num = int (input('숫자를 입력해주세요 > '))
+# print(num*2)
+
+
+#2
+# food = input('좋아하는 음식을 입력해주세요 > ')
+# print('좋아하는 음식 : ' + food)
+
+#3
+name = input('이름을 입력해주세요 > ')
+year = int(input('태어난 연도를 입력해주세요 > '))
+age = 2024-year
+
+print('제 이름은',name,'이고, 올해 나이는',age,'세 입니다' ) 
+
+# #4
+# first = input('첫 번째 문장을 입력해주세요 > ')
+# second = input('두 번째 문장을 입력해주세요 > ')
+
+# print(first+second)
+
+#5
+# num = int (input('숫자를 입력해주세요 > '))
+# if (num > 0) :
+#     print(num - num*2)
+# elif (num < 0):
+#     print (num - num*2)
+# else:
+#     print(0)
+
+#6
+# num1 = input ('첫 번째 정수형 숫자를 입력해주세요 > ')
+# num2 = input ('두 번째 정수형 숫자를 입력해주세요 > ')
+
+# print('더하기 연산 : ', int(num1) + int(num2))
+# print('빼기 연산 : ', int(num1)-int(num2))
+# print('곱하기 연산 : ', int(num1)*int(num2))
+# print('몫 연산 : ', int(num1)//int(num2))
+# print('나머지 연산 : ', int(num1)%int(num2))
+
+#7
+# num1 = input('첫 번째 정수형 숫자를 입력해주세요 > ')
+# num2 = input('두 번째 정수형 숫자를 입력해주세요 > ')
+# num3 = input('세 번째 정수형 숫자를 입력해주세요 > ')
+# sum = int(num1)+int(num2)+int(num3)
+# avg = int(sum / 3)
+
+# print(avg)
+
+#8
+# list_1 = []
+
+# num1 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+# list_1.append(num1)
+
+# num2 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+# list_1.append(num2)
+
+# num3 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+# list_1.append(num3)
+
+# num4 = int(input('네 번째 정수형 숫자를 입력해주세요 > '))
+# list_1.append(num4)
+
+# num5 = int(input('다섯 번째 정수형 숫자를 입력해주세요 > '))
+# list_1.append(num5)
+
+# print(list_1)
+
+# #9
+# str = input('문자열을 입력해주세요 > ')
+# num = int(input('정수형 숫자를 입력해주세요 > '))
+
+# print (str*num)
+
+#10
+# sum1 = int(input('첫 번째 정수형 숫자를 입력해주세요 > '))
+# print(sum1)
+
+# sum2 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+# print(sum1+sum2)
+
+# sum3 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+# print(sum1+sum2+sum3)
+
+# sum4 = int(input('두 번째 정수형 숫자를 입력해주세요 > '))
+# print(sum1+sum2+sum3+sum4)
+
+# sum5 = int(input('세 번째 정수형 숫자를 입력해주세요 > '))
+# print(sum1+sum2+sum3+sum4+sum5)
+
---
+++ 예제.py
@@ -0,0 +1,62 @@
+# 1
+# number1 = 1
+# number2 = number1 +1
+# print (number1, number2) # 1, 2
+
+# 주석처리 ctrl /
+
+# 2
+# number1 = 10
+# number2 = 5
+# number3 = str(number1) + str(number2)
+# number4 = number1+ number2
+
+# print(number1, number2, number3, number4) #10 5 105 15
+
+# 3
+# string = "Hello?"
+# n = 5
+
+# print(string*n) #Hello?Hello?Hello?Hello?Hello?
+
+# 4
+# string = "abc hello def"
+# sub_string1 = string[4:10]
+# sub_string2 = string[1:4]
+# sub_string3 = string[-1]
+
+# print(sub_string1) #hello 
+# print(sub_string2) #bc 
+# print(sub_string3) #f
+
+# 5
+# number1 = 5
+# number2 = 10.0 + number1
+# number1 - 5
+# number3 = number1 * (number2+10)
+
+# print(number1, number2, number3) #5 15.0 125.0
+
+# 6
+# list_variable = [1, 2, 3, [1, 2, 3]]
+# sub_list = list_variable
+
+# print(sub_list) #[1, 2, 3, [1, 2, 3]]
+
+# #7
+# list_variable =[]
+# list_variable.append(1)
+# list_variable.append("a")
+# list_variable[0] = 0
+
+# print(list_variable) #[0, 1, "a"]
+
+# 8
+name = input("너의 이름은?")
+print(name) #너의 이름은?
+
+# #9
+# age = int(input('너의 나이는?'))
+
+# print("올해 나이 : ", age) #올해 나이 : 25
+# print('내년 나이 :', age+1) #내년 나이 :26
